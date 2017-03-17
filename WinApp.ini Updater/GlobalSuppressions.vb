7bbbbc896e32cd414eb7984dcc73032dc6da0e5f
Tom <Tom@TOMS-DESKTOP>
2017-03-17 11:34:38 -0400

    Added more suppression code.

--- WinApp.ini Updater/GlobalSuppressions.vb
+++ WinApp.ini Updater/GlobalSuppressions.vb
@@ -17,3 +17,16 @@
 <Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope:="type", Target:="YAWA2_Updater.dataMissingException")>
 <Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope:="type", Target:="YAWA2_Updater.localFileAlreadyExistsException")>
 <Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope:="type", Target:="YAWA2_Updater.dataAlreadyExistsException")>
+<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2202:Do not dispose objects multiple times", Scope:="member", Target:="YAWA2_Updater.IniFile.#Save(System.String)")>
+<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2202:Do not dispose objects multiple times", Scope:="member", Target:="YAWA2_Updater.IniFile.#getRawINIText()")>
+<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2202:Do not dispose objects multiple times", Scope:="member", Target:="YAWA2_Updater.Check_for_Update_Code.#extractFileFromZIPFile(System.IO.MemoryStream&,System.String,System.String)")>
+<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2202:Do not dispose objects multiple times", Scope:="member", Target:="YAWA2_Updater.Form1.#Form1_Load(System.Object,System.EventArgs)")>
+<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2202:Do not dispose objects multiple times", Scope:="member", Target:="YAWA2_Updater.Form1.#applyNewINIFileSub()")>
+<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2202:Do not dispose objects multiple times", Scope:="member", Target:="YAWA2_Updater.Form1.#reDownloadSub()")>
+<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2202:Do not dispose objects multiple times", Scope:="member", Target:="YAWA2_Updater.programFunctions.functions.#trimINIFile(System.String,System.String,System.Boolean)")>
+<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2202:Do not dispose objects multiple times", Scope:="member", Target:="YAWA2_Updater.httpHelper.#downloadFile(System.String,System.IO.MemoryStream&,System.Boolean)")>
+<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2202:Do not dispose objects multiple times", Scope:="member", Target:="YAWA2_Updater.httpHelper.#downloadFile(System.String,System.String,System.Boolean,System.Boolean)")>
+<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2202:Do not dispose objects multiple times", Scope:="member", Target:="YAWA2_Updater.httpHelper.#getWebData(System.String,System.String&,System.Int16,System.Int16,System.Boolean)")>
+<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2202:Do not dispose objects multiple times", Scope:="member", Target:="YAWA2_Updater.httpHelper.#getWebData(System.String,System.String&,System.Boolean)")>
+<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2202:Do not dispose objects multiple times", Scope:="member", Target:="YAWA2_Updater.httpHelper.#uploadData(System.String,System.String&,System.Boolean)")>
+<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2202:Do not dispose objects multiple times", Scope:="member", Target:="YAWA2_Updater.httpHelper.#addPostDataToWebRequest(System.Net.HttpWebRequest&)")>
