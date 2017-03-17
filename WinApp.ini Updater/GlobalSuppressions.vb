89ea1035cd3d5ab0e6cfaf3fe4d19743be5c7c3c
Tom <Tom@TOMS-DESKTOP>
2017-03-17 11:09:29 -0400

    Added some Code Analysis suppression code.

---
+++ WinApp.ini Updater/GlobalSuppressions.vb
@@ -0,0 +1,18 @@
+﻿
+' This file is used by Code Analysis to maintain SuppressMessage 
+' attributes that are applied to this project.
+' Project-level suppressions either have no target or are given 
+' a specific target and scoped to a namespace, type, member, etc.
+
+<Assembly: CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles")>
+<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope:="type", Target:="YAWA2_Updater.dnsLookupError")>
+<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope:="type", Target:="YAWA2_Updater.credentialsAlreadySet")>
+<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope:="type", Target:="YAWA2_Updater.httpProtocolException")>
+<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope:="type", Target:="YAWA2_Updater.noSSLCertificateFoundException")>
+<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope:="type", Target:="YAWA2_Updater.sslErrorException")>
+<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope:="type", Target:="YAWA2_Updater.noHTTPServerResponseHeadersFoundException")>
+<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope:="type", Target:="YAWA2_Updater.proxyConfigurationErrorException")>
+<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope:="type", Target:="YAWA2_Updater.noMimeTypeFoundException")>
+<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope:="type", Target:="YAWA2_Updater.dataMissingException")>
+<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope:="type", Target:="YAWA2_Updater.localFileAlreadyExistsException")>
+<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope:="type", Target:="YAWA2_Updater.dataAlreadyExistsException")>
