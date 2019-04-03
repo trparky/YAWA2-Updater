3d564192b7e2a2fb2e69e921dbe93c7bf99a7c16
Tom <Tom@TOMS-DESKTOP>
2019-04-03 10:58:37 -0400

    Updated the version number.

--- WinApp.ini Updater/My Project/AssemblyInfo.vb
+++ WinApp.ini Updater/My Project/AssemblyInfo.vb
@@ -32,4 +32,4 @@ Imports System.Runtime.InteropServices
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
 <Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("1.6.2.0")>
+<Assembly: AssemblyFileVersion("1.7.1.0")>
