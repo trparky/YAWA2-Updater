6f1eb961f393313abb77bda1d10ebb8ec21f4f92
Tom <Tom@TOMS-DESKTOP>
2020-11-11 22:52:16 -0500

    Updated the code suppressions.

--- WinApp.ini Updater/GlobalSuppressions.vb
+++ WinApp.ini Updater/GlobalSuppressions.vb
@@ -4,29 +4,16 @@
 ' Project-level suppressions either have no target or are given 
 ' a specific target and scoped to a namespace, type, member, etc.
 
-<Assembly: CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles")>
 <Assembly: CodeAnalysis.SuppressMessage("Style", "IDE0017:Simplify object initialization")>
-<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope:="type", Target:="YAWA2_Updater.dnsLookupError")>
-<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope:="type", Target:="YAWA2_Updater.credentialsAlreadySet")>
-<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope:="type", Target:="YAWA2_Updater.httpProtocolException")>
-<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope:="type", Target:="YAWA2_Updater.noSSLCertificateFoundException")>
-<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope:="type", Target:="YAWA2_Updater.sslErrorException")>
-<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope:="type", Target:="YAWA2_Updater.noHTTPServerResponseHeadersFoundException")>
-<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope:="type", Target:="YAWA2_Updater.proxyConfigurationErrorException")>
-<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope:="type", Target:="YAWA2_Updater.noMimeTypeFoundException")>
-<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope:="type", Target:="YAWA2_Updater.dataMissingException")>
-<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope:="type", Target:="YAWA2_Updater.localFileAlreadyExistsException")>
-<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope:="type", Target:="YAWA2_Updater.dataAlreadyExistsException")>
-<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2202:Do not dispose objects multiple times", Scope:="member", Target:="YAWA2_Updater.IniFile.#Save(System.String)")>
-<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2202:Do not dispose objects multiple times", Scope:="member", Target:="YAWA2_Updater.IniFile.#getRawINIText()")>
-<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2202:Do not dispose objects multiple times", Scope:="member", Target:="YAWA2_Updater.Check_for_Update_Code.#extractFileFromZIPFile(System.IO.MemoryStream&,System.String,System.String)")>
-<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2202:Do not dispose objects multiple times", Scope:="member", Target:="YAWA2_Updater.Form1.#Form1_Load(System.Object,System.EventArgs)")>
-<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2202:Do not dispose objects multiple times", Scope:="member", Target:="YAWA2_Updater.Form1.#applyNewINIFileSub()")>
-<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2202:Do not dispose objects multiple times", Scope:="member", Target:="YAWA2_Updater.Form1.#reDownloadSub()")>
-<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2202:Do not dispose objects multiple times", Scope:="member", Target:="YAWA2_Updater.programFunctions.functions.#trimINIFile(System.String,System.String,System.Boolean)")>
-<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2202:Do not dispose objects multiple times", Scope:="member", Target:="YAWA2_Updater.httpHelper.#downloadFile(System.String,System.IO.MemoryStream&,System.Boolean)")>
-<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2202:Do not dispose objects multiple times", Scope:="member", Target:="YAWA2_Updater.httpHelper.#downloadFile(System.String,System.String,System.Boolean,System.Boolean)")>
-<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2202:Do not dispose objects multiple times", Scope:="member", Target:="YAWA2_Updater.httpHelper.#getWebData(System.String,System.String&,System.Int16,System.Int16,System.Boolean)")>
-<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2202:Do not dispose objects multiple times", Scope:="member", Target:="YAWA2_Updater.httpHelper.#getWebData(System.String,System.String&,System.Boolean)")>
-<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2202:Do not dispose objects multiple times", Scope:="member", Target:="YAWA2_Updater.httpHelper.#uploadData(System.String,System.String&,System.Boolean)")>
-<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2202:Do not dispose objects multiple times", Scope:="member", Target:="YAWA2_Updater.httpHelper.#addPostDataToWebRequest(System.Net.HttpWebRequest&)")>
+<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope:="type", Target:="~T:YAWA2_Updater.DnsLookupError")>
+<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope:="type", Target:="~T:YAWA2_Updater.CredentialsAlreadySet")>
+<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope:="type", Target:="~T:YAWA2_Updater.HttpProtocolException")>
+<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope:="type", Target:="~T:YAWA2_Updater.NoSSLCertificateFoundException")>
+<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope:="type", Target:="~T:YAWA2_Updater.sslErrorException")>
+<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope:="type", Target:="~T:YAWA2_Updater.noHTTPServerResponseHeadersFoundException")>
+<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope:="type", Target:="~T:YAWA2_Updater.proxyConfigurationErrorException")>
+<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope:="type", Target:="~T:YAWA2_Updater.noMimeTypeFoundException")>
+<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope:="type", Target:="~T:YAWA2_Updater.dataMissingException")>
+<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope:="type", Target:="~T:YAWA2_Updater.localFileAlreadyExistsException")>
+<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope:="type", Target:="~T:YAWA2_Updater.dataAlreadyExistsException")>
+<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2202:Do not dispose objects multiple times", Scope:="member", Target:="~M:YAWA2_Updater.HttpHelper.AddPostDataToWebRequest(System.Net.HttpWebRequest@)")>
