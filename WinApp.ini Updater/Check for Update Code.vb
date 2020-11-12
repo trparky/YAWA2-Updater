1644e92467e7b81382c97a3e7b4457307a4f6060
Tom <Tom@TOMS-DESKTOP>
2020-11-11 23:01:52 -0500

    Added some ReadOnly flags.

--- WinApp.ini Updater/Check for Update Code.vb
+++ WinApp.ini Updater/Check for Update Code.vb
@@ -130,9 +130,9 @@ Class Check_for_Update_Stuff
 
     Public windowObject As Form1
     Public Shared versionInfo As String() = Application.ProductVersion.Split(".")
-    Private shortBuild As Short = Short.Parse(versionInfo(VersionPieces.build).Trim)
+    Private ReadOnly shortBuild As Short = Short.Parse(versionInfo(VersionPieces.build).Trim)
     Public Shared versionString As String = String.Format("{0}.{1} Build {2}", versionInfo(0), versionInfo(1), versionInfo(2))
-    Private versionStringWithoutBuild As String = String.Format("{0}.{1}", versionInfo(VersionPieces.major), versionInfo(VersionPieces.minor))
+    Private ReadOnly versionStringWithoutBuild As String = String.Format("{0}.{1}", versionInfo(VersionPieces.major), versionInfo(VersionPieces.minor))
 
     Public Sub New(inputWindowObject As Form1)
         windowObject = inputWindowObject
