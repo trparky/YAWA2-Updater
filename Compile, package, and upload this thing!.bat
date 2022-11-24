5a9fa51595a0ac059bb0cda74d2727730375a167
Tom <Tom@TOMS-DESKTOP>
2022-11-23 22:10:53 -0500

    Added file to be ignored by GIT.

--- Compile, package, and upload this thing!.bat
+++
@@ -1,4 +0,0 @@
-MSBuild.exe "YAWA2 Updater.sln" /noconsolelogger /t:Rebuild /p:Configuration=Debug
-MSBuild.exe "YAWA2 Updater.sln" /noconsolelogger /t:Rebuild /p:Configuration=Release
-cd "WinApp.ini Updater\bin\Release"
-"Package This Program!.bat"
