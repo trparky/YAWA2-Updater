c67daaec4ea999c6f9b01e7f9e6e2e082105a6f7
Tom <Tom@TOMS-DESKTOP>
2023-03-27 18:55:19 -0400

    Updated HTTPHelper version to v1.331.

--- WinApp.ini Updater/Program Modules/HTTPHelper.vb
+++ WinApp.ini Updater/Program Modules/HTTPHelper.vb
@@ -202,7 +202,7 @@ End Class
 
 ''' <summary>Allows you to easily POST and upload files to a remote HTTP server without you, the programmer, knowing anything about how it all works. This class does it all for you. It handles adding a User Agent String, additional HTTP Request Headers, string data to your HTTP POST data, and files to be uploaded in the HTTP POST data.</summary>
 Public Class HttpHelper
-    Private Const classVersion As String = "1.329"
+    Private Const classVersion As String = "1.331"
 
     Private strUserAgentString As String = Nothing
     Private boolUseProxy As Boolean = False
@@ -863,7 +863,7 @@ beginAgain:
 
                 memStream.Write(dataBuffer, 0, lngBytesReadFromInternet) ' Writes the data directly to disk.
 
-                amountDownloaded = (currentFileSize / remoteFileSizeInput) * 100
+                amountDownloaded = currentFileSize / remoteFileSizeInput * 100
                 httpDownloadProgressPercentage = CType(Math.Round(amountDownloaded, 0), Short) ' Update the download percentage value.
                 DownloadStatusUpdateInvoker()
 
@@ -988,7 +988,7 @@ beginAgain:
 
                 fileWriteStream.Write(dataBuffer, 0, lngBytesReadFromInternet) ' Writes the data directly to disk.
 
-                amountDownloaded = (currentFileSize / remoteFileSizeInput) * 100
+                amountDownloaded = currentFileSize / remoteFileSizeInput * 100
                 httpDownloadProgressPercentage = CType(Math.Round(amountDownloaded, 0), Short) ' Update the download percentage value.
                 DownloadStatusUpdateInvoker()
 
